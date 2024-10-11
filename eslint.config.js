f5c77bf75129d9c6ac848183efd1f148dbf8ba54
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-10-11 21:56:56 +0100

    Vite + Vue + Typescript + eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import ts from 'typescript-eslint';
+export default [
+    { ignores: [ 'dist' ] },
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+    {
+        files: ['*.vue', '**/*.vue'],
+        languageOptions: {
+            parserOptions: {
+                parser: '@typescript-eslint/parser'
+            }
+        }
+    },
+    {
+        rules: {
+            semi: ['error', 'always'],
+            quotes: ['error', 'single', {
+                avoidEscape: true,
+                allowTemplateLiterals: true
+            }],
+            'eol-last': ['error', 'always']
+        }
+    }
+];
